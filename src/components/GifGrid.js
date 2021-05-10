2fb3a60a727d73e05a7062c9cd13f4cb632a9068
kndx99 <kendallsalazarvargas@gmail.com>
2021-05-09 18:03:23 -0600

    v1.0

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -11,7 +11,9 @@ export const GifGrid = ({ category }) => {
 	return (
 		<>
 			<h3>{category}</h3>
-			{loading && <p>Loading</p>}
+			{loading && (
+				<p className="animate__animated animate__flash">Loading</p>
+			)}
 			<div className="card-grid">
 				{images.map((img) => (
 					<GifGridItem key={img.id} {...img} />
