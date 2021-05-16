c1ffb50a32b9911afd25d55f6419ec00ae9c4dd2
Kendall Salazar MAC <kendallsalazarvargas@gmail.com>
2021-05-16 13:42:04 -0600

    v1.5

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,14 +5,15 @@ import PropTypes from 'prop-types';
  * Los unicos valores recibidor por parametro son title y url porque asi fue especificado al usar el operador spread
  */
 export const GifGridItem = ({ title, url }) => {
-	return (
-		<div className="card animate__animated animate__zoomIn">
-			<img src={url} alt={title} />
-			<p>{title}</p>
-		</div>
-	);
+    return (
+        <div className="card animate__animated animate__zoomIn">
+            <img src={url} alt={title} />
+            <p>{title}</p>
+        </div>
+    );
 };
+
 GifGridItem.propTypes = {
-	title: PropTypes.string.isRequired,
-	url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 };
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,32 +3,32 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en GifGridItem', () => {
-	let title = 'Este es solo un titulo de ejemplo';
-	let url = 'google.com';
-	let wrapper = shallow(<GifGridItem title={title} url={url} />);
+    let title = 'Este es solo un titulo de ejemplo';
+    let url = 'url';
+    let wrapper = shallow(<GifGridItem title={title} url={url} />);
 
-	test('Se debe de mostrar el componente correctamente', () => {
-		expect(wrapper).toMatchSnapshot();
-	});
+    test('Se debe de mostrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
 
-	test('El componente debe de tener un parrafo con el title', () => {
-		const p = wrapper.find('p');
-		expect(p.text()).toEqual(title);
-	});
+    test('El componente debe de tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text()).toEqual(title);
+    });
 
-	test('La imagen debe de tener el url y el alt segun los props', () => {
-		const img = wrapper.find('img');
-		expect(img.prop('src')).toEqual(url);
-		expect(img.prop('alt')).toEqual(title);
-	});
+    test('La imagen debe de tener el url y el alt segun los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toEqual(url);
+        expect(img.prop('alt')).toEqual(title);
+    });
 
-	test('La clase del div debe de contener una animacion', () => {
-		/**
-		 * Estas son 2 formas de hacer lo mismo, pero pueden tener aplicaciones diferentes en el futuro
-		 */
-		const div = wrapper.find('div');
-		const className = div.prop('className');
-		expect(className).toContain('animate__zoomIn');
-		expect(className.includes('animate__zoomIn')).toBe(true);
-	});
+    test('La clase del div debe de contener una animacion', () => {
+        /**
+         * Estas son 2 formas de hacer lo mismo, pero pueden tener aplicaciones diferentes en el futuro
+         */
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className).toContain('animate__zoomIn');
+        expect(className.includes('animate__zoomIn')).toBe(true);
+    });
 });
