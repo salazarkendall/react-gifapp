1f812f7594fe5db79a47149bdf4ed669a8d1c75a
Kendall Salazar MAC <kendallsalazarvargas@gmail.com>
2021-05-20 17:53:50 -0600

    v1.6

--- src/tests/components/GIfGrid.test.js
+++ src/tests/components/GIfGrid.test.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 import { shallow } from 'enzyme';
 import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
 
 describe('Pruebas en GifGrid', () => {
-	const category = '';
-	const wrapper = shallow(<GifGrid category={category} />);
+	const category = 'Death note';
 
 	test('Debe de renderizar el componente de manera adecuada', () => {
+		useFetchGifs.mockReturnValue({
+			data: [],
+			loading: true,
+		});
+
+		const wrapper = shallow(<GifGrid category={category} />);
 		expect(wrapper).toMatchSnapshot();
+		expect(wrapper.find('h3').text().includes(category)).toBe(true);
+	});
+
+	test('Debe de mostrar items cuando se cargan las imagenes usando useFetchGifs', () => {
+		const gifs = [
+			{
+				id: 'test',
+				title: 'Todo bien?',
+				url: 'https://localhost.hola',
+			},
+		];
+
+		useFetchGifs.mockReturnValue({
+			data: gifs,
+			loading: false,
+		});
+		const wrapper = shallow(<GifGrid category={category} />);
+		expect(wrapper).toMatchInlineSnapshot();
 	});
 });
